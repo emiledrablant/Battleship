b60b866c5f25d2d5a9664d6afbe7125ef95aac07
Emile Drablant <emiledrablant@proton.me>
2025-01-15 14:41:00 -0300

    add display for opponent's board

--- src/index.js
+++ src/index.js
@@ -1,35 +1,13 @@
 
 import "./style.css"
 import Player from "./modules/player";
-import { loadBoard } from "./modules/dom";
+import { loadBoard, loadBoardOpponent } from "./modules/dom";
 
 let player = new Player();
+let opponent = new Player();
+
+loadBoardOpponent(opponent);
+
 //loadBoard(player);
 player.data.placeShip(1,0,true,3);
 loadBoard(player);
-console.log(player.data.board);
-
-//addCell("abbbb");
-
-//createBoard();
-
-/*
-let a = new Gameboard;
-a.buildBoard();
-a.placeShip(0,5,true,2);
-a.placeShip(0,0,true,3);
-//console.log(a.board);
-a.receiveAttack(1,5);
-a.receiveAttack(1,6);
-a.receiveAttack(0,5);
-a.checkForGameOver();
-a.receiveAttack(0,0);
-a.receiveAttack(1,0);
-a.checkForGameOver();
-a.receiveAttack(2,0);
-a.checkForGameOver();
-/*
-let destroyer = new Ship(5);
-destroyer.hit();
-console.log(destroyer.numberOfHits);
-console.log(destroyer.isSunk());*/
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -2,8 +2,27 @@
 import Player from "./player";
 
 const playerBoard = document.getElementById("playerBoard");
+const opponentBoard = document.getElementById("opponentBoard");
 
 function loadBoard(player) {
+    for (let j = 0; j < 10; j++) {
+        for (let i = 0; i < 10; i++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+
+            const content = player.accessContent(i,j);
+            cell.textContent = content.charAt(0).toUpperCase();
+/*
+            cell.addEventListener("click", () => {
+                const result = player.data.board.get(player.data.cells[`${i},${j}`]);
+                console.log(result);
+            });*/
+            playerBoard.appendChild(cell);
+        }
+    }
+}
+
+function loadBoardOpponent(player) {
     for (let j = 0; j < 10; j++) {
         for (let i = 0; i < 10; i++) {
             const cell = document.createElement("div");
@@ -16,10 +35,10 @@ function loadBoard(player) {
                 const result = player.data.board.get(player.data.cells[`${i},${j}`]);
                 console.log(result);
             });
-            playerBoard.appendChild(cell);
+            opponentBoard.appendChild(cell);
         }
     }
 }
 
 
-export { loadBoard };
+export { loadBoard, loadBoardOpponent };
